057ec27e99e8c608064063ffc92343626aee56ae
extensaman <verus.wedding@gmail.com>
2022-03-14 21:32:33 +0300

    Task done (message correct)

--- src/main/java/by/academy/it/task02individual/task71/entity/Entity.java
+++ src/main/java/by/academy/it/task02individual/task71/entity/Entity.java
@@ -1,10 +1,13 @@
 package by.academy.it.task02individual.task71.entity;
 
 public class Entity {
+
+    public static final String MESSAGE = "Hello World!";
+
     private Entity() {
     }
 
     public static void printHelloWorld() {
-        System.out.println("Hello World!");
+        System.out.println(MESSAGE);
     }
 }
