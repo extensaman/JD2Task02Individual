9fa369b61f57d7bd2986ffe72c44b3ea63e37f77
extensaman <verus.wedding@gmail.com>
2022-03-14 20:36:58 +0300

    Task description added

--- src/main/java/by/academy/it/task02individual/task71/Runner.java
+++ src/main/java/by/academy/it/task02individual/task71/Runner.java
@@ -1,4 +1,15 @@
 package by.academy.it.task02individual.task71;
 
+/**
+ * Create a class with a printHelloWorld() method. Call a method using
+ * reflection from the main program.
+ *
+ * @author Yusikau Aliaksandr
+ * @version 1.0
+ */
+
 public class Runner {
+    public static void main(String[] args) {
+
+    }
 }
