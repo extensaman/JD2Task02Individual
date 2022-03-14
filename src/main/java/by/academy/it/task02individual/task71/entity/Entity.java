9c91cdcaa996aaa5d497583b19cc3314b880592c
extensaman <verus.wedding@gmail.com>
2022-03-14 21:30:45 +0300

    Task done

--- src/main/java/by/academy/it/task02individual/task71/entity/Entity.java
+++ src/main/java/by/academy/it/task02individual/task71/entity/Entity.java
@@ -1,5 +1,10 @@
 package by.academy.it.task02individual.task71.entity;
 
 public class Entity {
+    private Entity() {
+    }
 
+    public static void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
 }
